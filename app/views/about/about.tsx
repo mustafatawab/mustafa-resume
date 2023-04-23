9cc5e84fddd7bc461125781eccd3c4a5094e8308
Mustafa Tawab <115402517+mustafatawab@users.noreply.github.com>
2023-04-24 00:00:56 +0500

    Update about.tsx

--- app/views/about/about.tsx
+++ app/views/about/about.tsx
@@ -18,8 +18,8 @@ const About = () => {
               Thousands of clients have procured exceptional results while
               working with our dedicated team. when an unknown printer took a
               galley of type and scrambled it to make a type specimen book.
-              Delivering work within time and budget which meets client’s
-              requirements is our moto. Lorem Ipsum has been the industry's
+              Delivering work within time and budget which meets client
+              requirements is our moto. Lorem Ipsum has been the industry
               standard dummy text ever when an unknown printer took a galley.
             </p>
           </div>
